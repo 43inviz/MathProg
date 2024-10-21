6961c2fa97c1a71392960b9b73f8c6fadd65691b
Max Teplov <153938230+43inviz@users.noreply.github.com>
2024-10-21 13:41:59 +0200

    hw_07_10

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+
+
+namespace HW_07_10
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите мат. выражение");
+                string input = Console.ReadLine();
+
+                if(input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var result = CalculateExpression(input);
+                    Console.WriteLine($"Result: {result}");
+
+                }
+                catch (Exception ex) {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        static async Task<double> CalculateExpression(string expression)
+        {
+            var options = ScriptOptions.Default
+                .AddReferences (typeof(Math).Assembly)
+                .AddImports("System","SystemMath");
+
+            return await CSharpScript.EvaluateAsync<double>(expression, options);
+        }
+    }
+}
